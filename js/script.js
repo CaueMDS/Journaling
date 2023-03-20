03bf10cff3b451144d15bcca26293033c9a5576e
Cauê Miguel <caue.dantas2000@gmail.com>
2023-03-20 13:35:09 -0300

    página de anotações

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let Annotacion = document.querySelector('#annotations form')
+
+function openAnnotation(){
+    Annotacion.classList.add('open')
+}
+
+function closeAnnotation(){
+    Annotacion.classList.remove('open')
+}
